2bba9226f44175e1046365514d7280ee98e96ac6
Vaishnav C <vaishnav@kuliza.com>
2020-04-02 11:34:41 +0530

    Initial Commit

---
+++ CoreDataDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataDemo
+//
+//  Created by Vaishnav C on 4/2/20.
+//  Copyright © 2020 Vaishnav C. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
