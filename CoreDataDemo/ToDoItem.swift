99986e29ada9b52b244d895d454d13585f117f0e
Vaishnav C <vaishnav@kuliza.com>
2020-04-17 13:52:06 +0530

    CoreData

---
+++ CoreDataDemo/ToDoItem.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoItem.swift
+//  CoreDataDemo
+//
+//  Created by Vaishnav C on 4/3/20.
+//  Copyright © 2020 Vaishnav C. All rights reserved.
+//
+
+import Foundation
---
+++ CoreDataDemo/ToDoItemView.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoItemView.swift
+//  CoreDataDemo
+//
+//  Created by Vaishnav C on 4/6/20.
+//  Copyright © 2020 Vaishnav C. All rights reserved.
+//
+
+import SwiftUI
+
+struct ToDoItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoItemView()
+    }
+}
